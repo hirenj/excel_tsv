8446bab6999a7fb129032efa2337cc6fbc9aa740
Hiren Joshi <hirenj@gmail.com>
2015-02-11 11:01:22 +0100

    Make standalone executable files

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ import py2exe
 
 setup(
 	console=['extract_excel_sheet','write_excel'],
+	zipfile=None,
 	options={
 		'py2exe' : {
 			'packages' : [ 'xlrd','xlwt' ],
+			'dll_excludes':['w9xpopen.exe'],
+			"compressed" : 1,
+			"optimize" : 2,
+			"bundle_files" : 1
 		}
 	},
 	install_requires=[
